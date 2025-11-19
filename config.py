6461334efce486063666eb5ad84442163fead019
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-19 15:20:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,12 @@
 import os
-print(os.getenv("RAILWAY_DB_HOST") or os.getenv("MYSQL_HOST"))
-print(os.getenv("RAILWAY_DB_PORT") or os.getenv("MYSQL_PORT"))
-print(os.getenv("RAILWAY_DB_USER") or os.getenv("MYSQL_USER"))
-print(os.getenv("RAILWAY_DB_PASSWORD") or os.getenv("MYSQL_PASSWORD"))
-print(os.getenv("RAILWAY_DB_NAME") or os.getenv("MYSQL_DATABASE"))
-
 
 
 DB_CONFIG = 
     
-    "host": os.getenv("RAILWAY_DB_HOST") or os.getenv("MYSQL_HOST"),
+    "host": os.getenv("DB_HOST") or os.getenv("MYSQL_HOST"),
     
-    "port": int(os.getenv("RAILWAY_DB_PORT") or os.getenv("MYSQL_PORT")),
-    "user": os.getenv("RAILWAY_DB_USER") or os.getenv("MYSQL_USER"),
-    "password": os.getenv("RAILWAY_DB_PASSWORD") or os.getenv("MYSQL_PASSWORD"),
-    "database": os.getenv("RAILWAY_DB_NAME") or os.getenv("MYSQL_DATABASE"),
+    "port": int(os.getenv("DB_PORT") or os.getenv("MYSQL_PORT")),
+    "user": os.getenv("DB_USER") or os.getenv("MYSQL_USER"),
+    "password": os.getenv("DB_PASSWORD") or os.getenv("MYSQL_PASSWORD"),
+    "database": os.getenv("DB_NAME") or os.getenv("MYSQL_DATABASE"),
 }
