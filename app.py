5c74b04d3cabf33244e120644f1c14c744c60a1f
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-19 17:20:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import cloudinary
 import cloudinary.uploader
 
 cloudinary.config(
-    cloud_name="dnzkctdej",
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
