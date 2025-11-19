4cc28eb9ef081878aff418f40d8590acc37e523a
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-19 14:33:50 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
+print(os.getenv("RAILWAY_DB_HOST") or os.getenv("MYSQL_HOST"))
+int(os.getenv("RAILWAY_DB_PORT") or os.getenv("MYSQL_PORT"))
+print(os.getenv("RAILWAY_DB_USER") or os.getenv("MYSQL_USER"))
+print(os.getenv("RAILWAY_DB_PASSWORD") or os.getenv("MYSQL_PASSWORD"))
+print(os.getenv("RAILWAY_DB_NAME") or os.getenv("MYSQL_DATABASE"))
 
-DB_CONFIG = {
+
+
+DB_CONFIG = 
+    
     "host": os.getenv("RAILWAY_DB_HOST") or os.getenv("MYSQL_HOST"),
+    
     "port": int(os.getenv("RAILWAY_DB_PORT") or os.getenv("MYSQL_PORT")),
     "user": os.getenv("RAILWAY_DB_USER") or os.getenv("MYSQL_USER"),
     "password": os.getenv("RAILWAY_DB_PASSWORD") or os.getenv("MYSQL_PASSWORD"),
