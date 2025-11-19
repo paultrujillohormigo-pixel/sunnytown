c824fb594aa39252be72c8ef180e8dc0037d441b
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-19 16:38:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 import pymysql
 from config import DB_CONFIG
 import os
+import cloudinary
+import cloudinary.uploader
 
+cloudinary.config(
+    cloud_name="TU_CLOUD_NAME",
+    api_key="TU_API_KEY",
+    api_secret="TU_API_SECRET"
+)
 app = Flask(__name__)
 
 # Carpeta donde guardamos imágenes
