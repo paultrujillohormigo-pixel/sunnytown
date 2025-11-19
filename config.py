019dfdcf6fe21ee719da716eeba0b7af5900e027
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-19 15:26:30 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 DB_CONFIG = {
     
     "host": os.getenv("DB_HOST") or os.getenv("MYSQL_HOST"),
-    "port": int(os.getenv("DB_PORT") or os.getenv("MYSQL_PORT")),
+    "port": (os.getenv("DB_PORT") or os.getenv("MYSQL_PORT")),
     "user": os.getenv("DB_USER") or os.getenv("MYSQL_USER"),
     "password": os.getenv("DB_PASSWORD") or os.getenv("MYSQL_PASSWORD"),
     "database": os.getenv("DB_NAME") or os.getenv("MYSQL_DATABASE"),
