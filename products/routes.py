ff8397a8eb2e630bff4285ebfdb4c4d2b2533ea4
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-20 09:12:37 -0600

    Update routes.py

--- products/routes.py
+++ products/routes.py
@@ -49,7 +49,7 @@ def add_product():
         # Guardar en DB
         db = conectar_db()
         cursor = db.cursor()
-        sql = "INSERT INTO products (name, description, price, category, main_image,code VALUES (%s, %s, %s, %s, %s),%s"
+        sql = "INSERT INTO products (name, description, price, category, main_image,code VALUES (%s, %s, %s, %s, %s,%s)"
         cursor.execute(sql, (nombre, descripcion, precio, categoria, imagen_url,codigo))
         db.commit()
         cursor.close()
