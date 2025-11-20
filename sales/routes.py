f0a2ffd8f856fabcb05d2f1650074c6ef2520dc3
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-20 06:25:09 -0600

    Update routes.py

--- sales/routes.py
+++ sales/routes.py
@@ -1,1 +1,17 @@
 
+from flask import Blueprint, render_template, request
+
+sales_bp = Blueprint('sales', __name__, template_folder='templates')
+
+sales_bp = Blueprint('sales', __name__, template_folder='templates')
+
+@sales_bp.route('/sales')
+def list_sales():
+    return render_template('list_sales.html')
+
+@sales_bp.route('/sales/add', methods=['GET', 'POST'])
+def add_sale():
+    if request.method == 'POST':
+        # lógica para agregar venta
+        pass
+    return render_template('add_sale.html')
