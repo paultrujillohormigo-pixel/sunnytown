36ac7d3779b852fe56db5734b399e4d1e4c11495
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-20 07:17:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
-from products.routes import products_bp
+from flask import Flask, render_template, request
+from products.routes import products_bp, obtener_productos
 from sales.routes import sales_bp
 
 app = Flask(__name__)
-app.config.from_object('config')
 
 # Registrar blueprints
-app.register_blueprint(products_bp, url_prefix='/products')
-app.register_blueprint(sales_bp, url_prefix='/sales')
+app.register_blueprint(products_bp, url_prefix="/products")
+app.register_blueprint(sales_bp, url_prefix="/sales")
 
-if __name__ == '__main__':
+# Ruta principal: index
+@app.route("/", methods=["GET"])
+def index():
+    search_query = request.args.get("q") or ""
+    productos = obtener_productos(search_query)
+    return render_template("index.html", products=productos, search_query=search_query)
+
+if __name__ == "__main__":
     app.run(debug=True)
