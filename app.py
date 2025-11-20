c32ad8c7ba1ad9dd034f5d7fe689ee122b0e0b01
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-19 18:26:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import cloudinary
 import cloudinary.uploader
 
 cloudinary.config(
-    cloudinary_url=os.getenv("CLOUDINARY_URL")
+    cloudinary_url=os.getenv(cloudinary://848214456665939:GVuXnAr0TGjzImxusTE4Vk5M6QA@dnzkctdej)
 )
 
 app = Flask(__name__)
