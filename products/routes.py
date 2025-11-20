ca7cc12025917e564c817fbeb17f00c51d7410fb
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-20 06:24:45 -0600

    Update routes.py

--- products/routes.py
+++ products/routes.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, render_template, request
 
+products_bp = Blueprint('products', __name__, template_folder='templates')
+
+@products_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@products_bp.route('/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        # lógica para agregar producto
+        pass
+    return render_template('add_product.html')
