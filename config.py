074481cf944037f9e2802cea6a906bb29cc8b85f
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-20 19:50:42 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 
 DB_CONFIG = {
-    "host": "tramway.proxy.rlwy.net",
-    "port": 43803,
-    "user": "root",
-    "password": "ojRteBUenESTZqRFTNclOlfYhMQCKSDL",
-    "database": "railway"
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "port": int(os.environ.get("DB_PORT", 3306)),
+    "user": os.environ.get("DB_USER", "root"),
+    "password": os.environ.get("DB_PASSWORD", ""),
+    "database": os.environ.get("DB_NAME", "railway")
 }
