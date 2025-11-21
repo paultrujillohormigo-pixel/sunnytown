df1ffad54caa72b48c2a885a6b679bfbcab2f8e9
paultrujillohormigo-pixel <paul.trujillo.hormigo@gmail.com>
2025-11-20 18:51:36 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,9 @@ import os
 
 
 DB_CONFIG = {
-    
-    "host":"mysql-production-6c02.up.railway.app",
-    "port":3306,
-    "user":"root",
-    "password":"ojRteBUenESTZqRFTNclOlfYhMQCKSDL",
-    "database":"railway" ,
+    "host": "tramway.proxy.rlwy.net",
+    "port": 43803,
+    "user": "root",
+    "password": "ojRteBUenESTZqRFTNclOlfYhMQCKSDL",
+    "database": "railway"
 }
